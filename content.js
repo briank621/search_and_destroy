b319c0d8d26bbc9f33e7b427eada051bebfcff15
briank621 <bck2116@columbia.edu>
2016-02-06 11:57:22 -0500

    Added an icon as well as background scripts. Implemented the tutorial from https://robots.thoughtbot.com/how-to-make-a-chrome-extension

--- content.js
+++ content.js
@@ -1,3 +1,12 @@
-var myhref = $("a[href^='http']").eq(0).attr("href");
-console.log(myhref);
+// content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
 
+      console.log(firstHref);
+    }
+
+    chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+  }
+);
