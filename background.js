5a02635194972164027773b8a539281d0400e407
briank621 <bck2116@columbia.edu>
2016-02-06 12:42:04 -0500

    Changed manifest.json so that jquery loads first. Background will now load the words into the hash table

--- background.js
+++ background.js
@@ -1,20 +1,33 @@
 // background.js
 
+//initialize the words
+var dict = [];
+
+//reads the wordlist
+$.get('words', function(data) {
+	var lines = data.split("\n");
+	for (var i = 0, len = lines.length; i < len; i++) {
+		dict.push({key: lines[i],
+					value: "" });
+		//console.log(lines[i]);
+		}
+	}, 'text');
+
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  	var activeTab = tabs[0];
+  	chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
 
 
 //Opens a new tab based on content.js
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
-    }
-  }
-);
+	function(request, sender, sendResponse) {
+		if( request.message === "open_new_tab" ) {
+			chrome.tabs.create({"url": request.url});
+		}
+	}
+	);
