2d785d12774a043516dc1a80058de9eb0974c85a
briank621 <bck2116@columbia.edu>
2016-02-17 00:32:06 -0500

    Added a list for words to ignore. Added further logic as well as noticing when the application is inside tags<>

--- background.js
+++ background.js
@@ -1,17 +1,25 @@
 // background.js
 
 //initialize the words
-var dict = [];
-var seenWords = [];
+var dict = {};
+var seenWords = {};
+var skipwords = {};
 
 //reads the wordlist
 $.get('words', function(data) {
 	var lines = data.split("\n");
 	for (var i = 0, len = lines.length; i < len; i++) {
-		dict.push({key: lines[i],
-			value: "" });
-		//console.log(lines[i]);
-	}
+    dict[lines[i]] = 1;
+  }
+}, 'text');
+
+//reads common words
+//reads the wordlist
+$.get('skip_words', function(data) {
+  var lines = data.split("\n");
+  for (var i = 0, len = lines.length; i < len; i++) {
+    skipwords[lines[i]] = 1;
+  }
 }, 'text');
 
 //Listens for words to replace
@@ -19,24 +27,50 @@ chrome.runtime.onMessage.addListener(
 	function(message, sender, sendResponse) {
 		var ans = ""; //the string to return
 		if (message.type == "request"){
-    		var text = message.content;
+      var text = message.content;
     		//console.log(message.content);
     		var words = text.split(" ");
-    		for (var i = 0, len = words.length; i < len; i++) {
+        var seenLeft = 0; //checks for tags
+        for (var i = 0, len = words.length; i < len; i++) {
     			//check if it contains only letters
     			var word = words[i];
-    			var word = "REPLACED";
-    			if(/^[a-zA-Z]+$/.test(word))
-    				ans += "<span class='highlight-replace'>" + word + " </span>";
-    			else
-    				ans += word + " ";
-    		}
-    	}
-    	var resp = {type: "response", content: ans};
-    	sendResponse(resp);
-    	return true;
-	}
-);
+          //lowercase word and check
+          var first = word.charAt(0);
+          var last = word.charAt(word.length - 1);
+          var lower = true;
+          var check_word = word;
+          if(first != word.toLowerCase().charAt(0)){
+            lower = false;
+            check_word = word.toLowerCase();
+          }
+          if(first === "<"){
+            seenLeft += 1;
+            ans += word + " ";
+            continue;
+          }
+          if(last === ">"){
+            seenLeft -= 1;
+            ans += word + " ";
+            continue;
+          }
+          if(seenLeft > 0){
+            ans += word + " ";
+            continue;
+          }
+          //check if word can be skipped
+          if(skipwords[check_word] === 1 || !/^[a-zA-Z]+$/.test(word)){
+            //console.log(check_word);
+            ans += word + " ";
+            continue;
+          }
+          ans += "<span class='highlight-replace'>" + word + "</span> ";
+        }
+      }
+      var resp = {type: "response", content: ans};
+      sendResponse(resp);
+      return true;
+    }
+    );
 
 
 // Called when the user clicks on the browser action.
